8d46bb42da390221423ae9d9a4d0c1b995335ce7
lgfh98 <lgfigueroah@outlook.com>
2021-10-11 16:52:12 -0500

    c

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 
 import App from './routes/App';
 
+console.log('...');
+
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
@@ -19,7 +19,7 @@ const App = () => {
   const initialState = useInitialState();
   return (
     <AppContext.Provider value={initialState}>
-      <BrowserRouter>
+      <HashRouter>
         <Layout>
           <Switch>
             <Route exact path="/" component={Home} />
@@ -30,7 +30,7 @@ const App = () => {
             <Route component={NotFound} />
           </Switch>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </AppContext.Provider>
   );
 };
